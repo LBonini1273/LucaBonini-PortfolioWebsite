d8272f6d50dddf9d3cf6a7267aa9be390fd717ca
LBonini1273 <luca.bonini1273@gmail.com>
2024-12-09 11:52:49 -0500

    Update page.tsx
    Properly indent for readability and added proper CSS class to all the CD states

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,35 +37,37 @@ function TVSet() {
 
     return (
       <div className={styles.tvset}>
-      <div id='tvback' key={activeProj} className={styles.tvback}>
-        <h1>
-          <div className={styles.projcopy}>
-            <p className={styles.projtext}>I understand how contentious it can be to list one&apos;s portfolio website as a project, but that is exactly why I did my best to create something from scratch that was uniquely my own.</p>
-            <p className={styles.projtext}>I designed the layout myself and used HTML, CSS, React, and Three to create the frontend. I then used Next.js for the backend and Vercel to host the website. 
-            I also used libraries such as React Three Fiber, Drei, and Motion for React to add additional features such as the animations.</p>
-            <p className={styles.projtext}>Not only was this an opportunity for me to refine my exsisting knowledge, but I was also able to learn a lot of new things in the process.
-            Be sure to checkout my GitHub repo to see how it evolves over time.</p> 
+        <div id='tvback' key={activeProj} className={styles.tvback}>
+          <h1>
+            <div className={styles.projcopy}>
+              <p className={styles.projtext}>I understand how contentious it can be to list one&apos;s portfolio website as a project, but that is exactly why I did my best to create something from scratch that was uniquely my own.</p>
+              <p className={styles.projtext}>I designed the layout myself and used HTML, CSS, React, and Three to create the frontend. I then used Next.js for the backend and Vercel to host the website. 
+              I also used libraries such as React Three Fiber, Drei, and Motion for React to add additional features such as the animations.</p>
+              <p className={styles.projtext}>Not only was this an opportunity for me to refine my exsisting knowledge, but I was also able to learn a lot of new things in the process.
+              Be sure to checkout my GitHub repo to see how it evolves over time.</p> 
+            </div>
+            <a className={styles.repolink} href='https://github.com/LBonini1273/LucaBonini-PortfolioWebsite' target="_blank"/>
+            <Image width={200} height={200} className={styles.imgs} src="/github.png" alt="" />
+            <div className={styles.tvstatic}></div>
+          </h1>
+        </div>
+        <div className={styles.canvbox}> 
+          <div className={styles.canv}>
+            <Canvas>
+            <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} /> 
+            <ambientLight intensity={0.8} />
+            <pointLight intensity={1} position={[0, 6, 0]} />
+            <Html position={[2, 6.1, 2]}>
+              <div className={styles.consolelogo}>ProjBox</div>
+            </Html>
+            <Shelf />
+            <Eject onShow={handleClick} />
+            <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
+            <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
+            <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/> 
+            </Canvas>
           </div>
-          <a className={styles.repolink} href='https://github.com/LBonini1273/LucaBonini-PortfolioWebsite' target="_blank"/>
-          <Image width={200} height={200} className={styles.imgs} src="/github.png" alt="" />
-          <div className={styles.tvstatic}></div>
-        </h1>
-      </div>  
-      <div className={styles.canv}>
-      <Canvas>
-      <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} /> 
-      <ambientLight intensity={0.8} />
-      <pointLight intensity={1} position={[0, 6, 0]} />
-      <Html position={[2, 6.1, 2]}>
-        <div className={styles.consolelogo}>ProjBox</div>
-      </Html>
-      <Shelf />
-      <Eject onShow={handleClick} />
-      <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
-      <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
-      <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/> 
-      </Canvas>
-      </div>
+        </div> 
       </div>
     )
   }
@@ -77,27 +79,29 @@ function TVSet() {
     
     return (
       <div className={styles.tvset}>
-      <div id='tvback' key={activeProj} className={styles.tvback}>
-        <h1>
-          <p className={styles.tempcopy}>Please try a different disc. We can&apos;t play this one.</p>
-          <div className={styles.tvstatic}></div>
-        </h1>
-      </div>  
-      <div className={styles.canv}>
-      <Canvas>
-      <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} />
-      <ambientLight intensity={0.8} />
-      <pointLight intensity={1} position={[0, 6, 0]} />
-      <Html position={[2, 6.1, 2]}>
-        <div className={styles.consolelogo}>ProjBox</div>
-      </Html>
-      <Shelf />
-      <Eject onShow={handleClick} />
-      <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
-      <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
-      <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/>    
-      </Canvas>
-      </div>
+        <div id='tvback' key={activeProj} className={styles.tvback}>
+          <h1>
+            <p className={styles.tempcopy}>Please try a different disc. We can&apos;t play this one.</p>
+            <div className={styles.tvstatic}></div>
+          </h1>
+        </div>
+        <div className={styles.canvbox}> 
+          <div className={styles.canv}>
+            <Canvas>
+            <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} />
+            <ambientLight intensity={0.8} />
+            <pointLight intensity={1} position={[0, 6, 0]} />
+            <Html position={[2, 6.1, 2]}>
+              <div className={styles.consolelogo}>ProjBox</div>
+            </Html>
+            <Shelf />
+            <Eject onShow={handleClick} />
+            <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
+            <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
+            <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/>    
+            </Canvas>
+          </div>
+        </div> 
       </div>
     )
   }
@@ -109,53 +113,55 @@ function TVSet() {
     
     return (
       <div className={styles.tvset}>
-      <div id='tvback' key={activeProj} className={styles.tvback}>
-        <h1>
-        <p className={styles.tempcopy}>Please try a different disc. We can&apos;t play this one.</p>
-          <div className={styles.tvstatic}></div>
-        </h1>
-      </div>  
-      <div className={styles.canv}>
-      <Canvas>
-      <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} />
-      <ambientLight intensity={0.8} />
-      <pointLight intensity={1} position={[0, 6, 0]} />
-      <Html position={[2, 6.1, 2]}>
-        <div className={styles.consolelogo}>ProjBox</div>
-      </Html>
-      <Shelf />
-      <Eject onShow={handleClick} />
-      <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
-      <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
-      <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/>      
-      </Canvas>
-      </div>
+        <div id='tvback' key={activeProj} className={styles.tvback}>
+          <h1>
+            <p className={styles.tempcopy}>Please try a different disc. We can&apos;t play this one.</p>
+            <div className={styles.tvstatic}></div>
+          </h1>
+        </div>
+        <div className={styles.canvbox}> 
+          <div className={styles.canv}>
+            <Canvas>
+            <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} />
+            <ambientLight intensity={0.8} />
+            <pointLight intensity={1} position={[0, 6, 0]} />
+            <Html position={[2, 6.1, 2]}>
+              <div className={styles.consolelogo}>ProjBox</div>
+            </Html>
+            <Shelf />
+            <Eject onShow={handleClick} />
+            <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
+            <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
+            <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/>      
+            </Canvas>
+          </div>
+        </div>
       </div>
     )
   }
 
   return (
     <div className={styles.tvset}>
-    <div id='tvback' className={styles.tvback}>
-      <h1></h1>
-    </div>
-    <div className={styles.canvbox}>
-      <div className={styles.canv}>
-        <Canvas> 
-        <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} /> 
-        <ambientLight intensity={0.8} />
-        <pointLight intensity={1} position={[0, 6, 0]} />
-        <Html position={[2, 6.1, 2]}>
-          <div className={styles.consolelogo}>ProjBox</div>
-        </Html>
-        <Shelf />
-        <Eject onShow={handleClick} />
-        <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
-        <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
-        <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/> 
-        </Canvas>
+      <div id='tvback' className={styles.tvback}>
+        <h1></h1>
       </div>
-    </div>
+      <div className={styles.canvbox}>
+        <div className={styles.canv}>
+          <Canvas> 
+          <OrthographicCamera makeDefault position={[0, 0, 9]} zoom={27.3} /> 
+          <ambientLight intensity={0.8} />
+          <pointLight intensity={1} position={[0, 6, 0]} />
+          <Html position={[2, 6.1, 2]}>
+            <div className={styles.consolelogo}>ProjBox</div>
+          </Html>
+          <Shelf />
+          <Eject onShow={handleClick} />
+          <CDR onShow={() => setActiveProj(1)} x={-11.4} y={-2.5} z={4} active={p1Active} activenum={1}/>
+          <CDR onShow={() => setActiveProj(2)} x={0.6} y={-2.5} z={4} active={p2Active} activenum={2}/>
+          <CDR onShow={() => setActiveProj(3)} x={12.6} y={-2.5} z={4} active={p3Active} activenum={2}/> 
+          </Canvas>
+        </div>
+      </div>
     </div>
   )    
 }
