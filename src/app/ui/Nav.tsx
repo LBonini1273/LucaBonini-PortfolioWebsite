253080b4a8d56bd0cda5847d9786bbb7d72a8902
LucaBonini <luca.bonini1273@gmail.com>
2025-05-08 12:30:36 -0400

    Delete Framer Motion

--- src/app/ui/Nav.tsx
+++ src/app/ui/Nav.tsx
@@ -1,24 +1,23 @@
-import { motion } from "framer-motion"
 import styles from '@/app/page.module.css'
 
 export default function Nav() {
     return (
         <nav>
-            <motion.div id="about" className={styles.about} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{delay:7.5, duration:0.25}}>
+            <div id="about" className={styles.about}>
                 <a href="#">
                     <p>About Me</p>
                 </a>
-            </motion.div>
-            <motion.div id="proj" className={styles.proj} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{delay:8, duration:0.25}}>
+            </div>
+            <div id="proj" className={styles.proj}>
                 <a href="#projects">
                     <p>Projects</p>
                 </a>
-            </motion.div>
-            <motion.div id="links" className={styles.links} initial={{ opacity: 0, scale: 0 }} animate={{ opacity: 1, scale: 1 }} transition={{delay:8.5, duration:0.25}}>
+            </div>
+            <div id="links" className={styles.links}>
                 <a href="#linksection">
                     <p>Links</p>
                 </a>
-            </motion.div>
+            </div>
         </nav>
         )
     }
