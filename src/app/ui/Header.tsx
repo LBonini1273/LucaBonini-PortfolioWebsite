1e03b3a01b4aa61dad807b253f3960a069231419
LucaBonini <luca.bonini1273@gmail.com>
2025-05-08 12:01:52 -0400

    Update Package

--- src/app/ui/Header.tsx
+++ src/app/ui/Header.tsx
@@ -1,4 +1,3 @@
-import { motion } from "framer-motion"
 import styles from '@/app/page.module.css'
 import Image from "next/image"
 export default function Header() {
@@ -6,27 +5,27 @@ export default function Header() {
     return (
         <header>
             {/*Intro for PC Screens*/}
-            <motion.div className={styles.lJoycon} initial={{ y:-1000 }} transition={{delay:4}} animate={{ y:0 }}>
+            <div className={styles.lJoycon}>
 
-            </motion.div>
-            <motion.div className={styles.screen} initial={{ y:-1000 }} transition={{delay:1, duration:2}} animate={{ y:0 }}>
-                <motion.div className={styles.picdiv} initial={{ x:-450 }} transition={{delay:6, duration:1}} animate={{ x:0 }}>
-                    <motion.img className={styles.me} src="/OnlygoodpicIhaveedit.jpg" alt=""/>
-                </motion.div>
+            </div>
+            <div className={styles.screen}>
+                <div className={styles.picdiv}>
+                    <img className={styles.me} src="/OnlygoodpicIhaveedit.jpg" alt=""/>
+                </div>
                 
-                <motion.div className={styles.aboutcopy} tabIndex={0} initial={{ y:-450 }} transition={{delay:6, duration:1}} animate={{ y:0 }}>
+                <div className={styles.aboutcopy} tabIndex={0}>
                     <p className={styles.abouttext}>Hello there, I&apos;m Luca Bonini and I am a Web Developer looking to expand my knowledge and experience in the field. </p>
                     <p className={styles.abouttext}>My passion for using my creativity both in an artistic and problem-solving sense makes me an invaluable asset to any team, and my strong drive to further my skills and knowledge on my own time makes me a worthwhile investment. </p>
                     <p className={styles.abouttext}>If the theme of my website didn&apos;t give it away I love playing video games as well as reading, listening to music, and in general just learning and trying new things.</p>
-                </motion.div>
-                <motion.div className={styles.barbox} initial={{ y:-450 }} transition={{delay:6, duration:1}} animate={{ y:0 }}>
+                </div>
+                <div className={styles.barbox}>
                     <div className={styles.lredbar}/>
                     <div className={styles.rredbar}/>
-                </motion.div>
-            </motion.div>
-            <motion.div className={styles.rJoycon} initial={{ y:-1000 }} transition={{delay:4}} animate={{ y:0 }}>
+                </div>
+            </div>
+            <div className={styles.rJoycon}>
 
-            </motion.div>
+            </div>
 
             {/*Intro for Mobile Screens*/}
             <div className={styles.gboyscreen}>
